d0b9c24226a7d10418845303cbf8a36bf7def832
Michal Sroka <misroka@microsoft.com>
2022-09-30 23:40:24 +0200

    First version of the Home automation project
    Add file and requirements to gather information from all files.

---
+++ PowerMonitor/pull_sensors.py
@@ -0,0 +1,82 @@
+import tuyapower
+import json
+import time
+import sys
+from azure.data.tables import TableClient
+from azure.core.exceptions import ResourceExistsError, HttpResponseError
+
+MIN_WAIT_TIME_S = 5
+SNAPSHOT_FILE_NAME=sys.argv[1] if len(sys.argv) >= 2 else "snapshot.json"
+AZURE_CONFIG_FILE_NAME=sys.argv[2] if len(sys.argv) >= 3 else 'azure_config.json'
+
+
+# Read and parse JSON file
+def read_json_file(json_file):
+    with open(json_file) as json_data:
+        data = json.load(json_data)
+        return data
+
+def  read_azure_storage_account_info(filename = 'azure_config.json'):
+    with open(filename) as json_data:
+        data = json.load(json_data)
+    return data.get('connection_string'), data.get('table_name')
+
+# Create entity on Azure Table storage.
+def create_entity(table_client, entity_data):
+    try:
+        resp = table_client.create_entity(entity=entity_data)
+        print(resp)
+    except ResourceExistsError:
+        print("Entity already exists")
+
+
+
+
+sensors_data = read_json_file(SNAPSHOT_FILE_NAME)
+
+CONNECTION_STRING, TABLE_NAME = read_azure_storage_account_info(AZURE_CONFIG_FILE_NAME)
+
+sensor_last_read = {}
+
+with TableClient.from_connection_string(CONNECTION_STRING, TABLE_NAME) as table_client:
+    # Create a table in case it does not already exist
+    try:
+        table_client.create_table()
+    except HttpResponseError:
+        print("Table already exists")
+
+    while True:
+        for sensor in sensors_data["devices"]:
+            start_timestamp = time.time()
+            sensor_name = sensor["name"]
+            
+            # Get the last read time for this sensor, and check if it is too soon to read again.
+            if sensor_name in sensor_last_read.keys():
+                difference = start_timestamp - sensor_last_read[sensor_name]
+                if difference < MIN_WAIT_TIME_S:
+                    # Skipping sensor as it was read less than Min_Wait_Time seconds ago
+                    time.sleep(MIN_WAIT_TIME_S - difference)
+                    continue
+
+            ## Update when the sensor was last read to current time.
+            sensor_last_read[sensor_name] = start_timestamp
+            (on, w, mA, V, err) = tuyapower.deviceInfo(sensor["id"],  sensor["ip"], sensor["key"], sensor["ver"])
+            
+            # Get current time
+            current_time = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
+            
+            # Create a new entity
+            entity = {
+            "PartitionKey": sensor["name"],
+            "RowKey": f"{sensor['name']}_{current_time}",
+            "timestamp": start_timestamp,
+            "name": sensor["name"],
+            "isOn": on,
+            "w": w,
+            "mA": mA,
+            "V": V,
+            "err": err
+            }
+            # Insert the entity into the Table
+            create_entity(table_client, entity)
+
